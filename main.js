9209f77e84d392ae786c8300ad32ab52ec824c41
Valentino <devalen543@gmail.com>
2025-01-04 11:17:54 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 const form = document.getElementById("form-clima");
 const input = document.getElementById("busqueda");
 const tarjetaClima = document.getElementById("seccion-clima");
@@ -10,7 +9,7 @@ form.addEventListener("submit", (event) => {
     const ciudad = input.value.trim();  // Quitamos espacios en blanco
     
     // Si ciudad es un input válido, chequeamos si existe en la API
-    if (ciudad) { fetchClima(ciudad); } else { alert("Ingrese un nombre de ciudad"); }
+    if (ciudad) { fetchClima(ciudad); } else { alert("INGRESE UN NOMBRE DE CIUDAD"); }
 
 });
 
@@ -18,14 +17,21 @@ form.addEventListener("submit", (event) => {
 function fetchClima(ciudad) {
 
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=1c2068f68dd955543784abc4d3496e9c`)
-        
-    .then((response) => response.json())
-        
-    .then((data) => funTarjetaClima(data))
+            
+    .then((response) => {
+
+        // .ok es un bool que devuelve true si el código de estado HTTP de la respuesta está en el rango 200-299. False caso contrario
+        if (!response.ok) { throw new Error("Ciudad no encontrada"); }
+                
+        return response.json(); })
+
+    .then((data) => renderTarjetaClima(data))
+            
+    .catch((error) => { alert("COULDN'T FIND THE CITY. TRY AGAIN"); });
 }
+    
 
-
-function funTarjetaClima(data) {
+function renderTarjetaClima(data) {
 
     // Creo el div del cuadro con contenido
     const cuadro_clima = document.createElement("div");
@@ -36,12 +42,30 @@ function funTarjetaClima(data) {
     tarjetaClima.className = "seccion-clima";   // Le agrego la clase para el style
     tarjetaClima.appendChild(cuadro_clima);
 
+    // Sección icono del clima - llamamos a la función que devuelve el icono
+    let imgWeather = iconoClima(data.weather[0].description);
 
-// Sección icono del clima
-let imgWeather;
 
-// Mapa de iconos del clima
-const weatherIcons = {
+    // Insertamos datos del clima obtenidos por la API
+    cuadro_clima.innerHTML = 
+    `
+        <div class="ciudad-clima">
+            <p class="nombre-clima">${data.name}, ${data.sys.country}</p>
+            <img class="icono-clima" src="${imgWeather}" alt="Icono de ${data.weather[0].description}">
+        </div>
+        <div class="datos-clima">
+            <p class="tipo-clima">Climate: ${data.weather[0].description}</p>
+            <p class="temp-clima">Temperature: ${(data.main.temp - 273.15).toFixed(1)} °C</p>
+            <p class="hum-clima">Humidity: ${data.main.humidity}%</p>
+        </div>
+    `;
+}
+
+
+function iconoClima(descripcion) {
+
+    // Mapa de iconos del clima - nombres obtenidos de la API
+    const mapaIconosClima = {
 
     "clear sky": "img/soleado.png",
     "few clouds": "img/dispersas.png",
@@ -53,29 +77,10 @@ const weatherIcons = {
     "shower rain": "img/nublado.png",
     "rain": "img/lluvia.png",
     "thunderstorm": "img/tormenta.png",
-    "snow": "",
-    "mist": ""
-};
+    "snow": "img/nieve.png",
+    "mist": "img/niebla.png"
+    };
 
-// En cada llamada, se asigna el icono basado en la descripción del clima dada por la API
-imgWeather = weatherIcons[data.weather[0].description] || "img/default.png"; // Default si no coincide
-
-
-    // Insertamos datos del clima obtenidos por la API
-
-    cuadro_clima.innerHTML = 
-    `
-        <div class="ciudad-clima">
-            <p class="nombre-clima">${data.name}, ${data.sys.country}</p>
-            <img class="icono-clima" src="${imgWeather}" alt="icono del clima">
-        </div>
-        <div class="datos-clima">
-            <p class="tipo-clima">Climate: ${data.weather[0].description}</p>
-            <p class="temp-clima">Temperature: ${(data.main.temp - 273.15).toFixed(1)} °C</p>
-            <p class="hum-clima">Humidity: ${data.main.humidity}%</p>
-        </div>
-    `;
+    return mapaIconosClima[descripcion] || "img/default.png";
 
 }
-
-
